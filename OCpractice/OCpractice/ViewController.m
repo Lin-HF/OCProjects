0ca69d33f7fc78b5b8033179d9729f86c61a57ac
David Lin <haiflin@iu.edu>
2018-12-03 22:41:24 -0500

    OC Learning

---
+++ OCpractice/OCpractice/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  OCpractice
+//
+//  Created by David on 2018/12/3.
+//  Copyright © 2018 David. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
